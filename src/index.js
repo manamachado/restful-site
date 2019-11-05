951fd1b72b29a1358dd29bccecf535a5c556d56b
Maná Machado <manasses.machado@luizalabs.com>
2019-11-04 22:27:15 -0300

    first ;)

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const request = require('request-promise-native');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+app.get('/produtos', async (req, res) => {
+  const result = await request.get('http://localhost:3000/api/v1/products');
+  const products = JSON.parse(result).data;
+  res.render('products', { products });
+})
+
+app.listen(3001, () => {
+  console.log('\x1b[33m%s\x1b[0m', '========== Server running on port 3001 ==========');
+})
